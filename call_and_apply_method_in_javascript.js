a3daba00043f942db4561de05072352fe7df2b40
Vijay Rajagopal <vijayrajagopal996@gmail.com>
2019-12-29 10:14:10 +0000

    #

--- call_and_apply_method_in_javascript.js
+++ call_and_apply_method_in_javascript.js
@@ -11,6 +11,6 @@ const sally = {
   console.log(money);
 }
 
-console.log(billy.transferAmount.call(sally,30));
+billy.transferAmount.call(sally,30);
 
 /* call is used to borrow or to reuse functions.
--- currying_using_bind_function.js
+++
@@ -1,6 +0,0 @@
-function multiply(a,b){
-      return a % b == 0 ? 'It is Even':'Not Even';
-}
-
-let executeMultiply = multiply.bind(this,10);
-console.log(executeMultiply(11));
